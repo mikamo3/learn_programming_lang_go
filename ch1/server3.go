909c4d6629260a078df21192b2ed5176a85a9564
mikamo3 <kamo3proj@gmail.com>
2022-09-01 15:30:19 +0000

    P28

---
+++ ch1/server3.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/l", func(w http.ResponseWriter, r *http.Request) {
+		cycle := 20
+		if err := r.ParseForm(); err != nil {
+		}
+		for k, v := range r.Form {
+			if k == "cycles" {
+				c, err := strconv.Atoi(v[0])
+				if err != nil {
+					fmt.Fprintf(w, "%q,%q", k, v)
+				}
+				cycle = c
+			}
+		}
+		lissajous(w, cycle)
+	})
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] =%q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "URL.path=%q\n", r.URL.Path)
+}
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
+
+var palette = []color.Color{color.Black, color.White, color.RGBA{0x00, 0xFF, 0x00, 0xff}, color.RGBA{0xff, 0x00, 0x00, 0xff}, color.RGBA{0x00, 0x00, 0xff, 0xff}}
+
+const (
+	whiteIndex = 1
+	blackIndex = 0
+	greenIndex = 2
+	redIndex   = 3
+	blueIndex  = 4
+)
+
+func lissajous(out io.Writer, cycle int) {
+	const (
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycle)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(i%3+2))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
