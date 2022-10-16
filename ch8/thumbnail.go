2adb8e8fad2e01c9126d829d69152bfa97ebf54b
mikamo3 <kamo3proj@gmail.com>
2022-10-16 12:44:29 +0000

    P276

---
+++ ch8/thumbnail.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sync"
+
+	"gopl.io/ch8/thumbnail"
+)
+
+func makeThumbnails(filenames []string) {
+	for _, f := range filenames {
+		if _, err := thumbnail.ImageFile(f); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func makeThumbnails2(filenames []string) {
+	for _, f := range filenames {
+		go thumbnail.ImageFile(f)
+	}
+}
+
+func makeThumbnails3(filenames []string) {
+	ch := make(chan struct{})
+	for _, f := range filenames {
+		go func(f string) {
+			thumbnail.ImageFile(f)
+			ch <- struct{}{}
+		}(f)
+	}
+	for range filenames {
+		<-ch
+	}
+}
+
+func makeThumbnails4(filenames []string) error {
+	errors := make(chan error)
+	for _, f := range filenames {
+		go func(f string) {
+			_, err := thumbnail.ImageFile(f)
+			errors <- err
+		}(f)
+	}
+	for range filenames {
+		if err := <-errors; err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func makeThumbnails5(filenames []string) (thumbfiles []string, err error) {
+	type item struct {
+		thubfile string
+		err      error
+	}
+	ch := make(chan item, len(filenames))
+	for _, f := range filenames {
+		go func(f string) {
+			var it item
+			it.thubfile, it.err = thumbnail.ImageFile(f)
+			ch <- it
+		}(f)
+	}
+	for range filenames {
+		it := <-ch
+		if it.err != nil {
+			return nil, err
+		}
+		thumbfiles = append(thumbfiles, it.thubfile)
+	}
+	return thumbfiles, nil
+}
+
+func makeThumbnails6(filenames <-chan string) int64 {
+	sizes := make(chan int64)
+	var wg sync.WaitGroup
+	for f := range filenames {
+		wg.Add(1)
+		go func(f string) {
+			defer wg.Done()
+			thumb, err := thumbnail.ImageFile(f)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			info, _ := os.Stat(thumb)
+			sizes <- info.Size()
+		}(f)
+		go func() {
+			wg.Wait()
+			close(sizes)
+		}()
+		var total int64
+		for size := range sizes {
+			total += size
+		}
+		return total
+	}
+}
